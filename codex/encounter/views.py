6f15cb8094d500be11601c6cda42cc488a1a0fc1
ghilbrae <elwen@elenya.net>
2012-05-19 17:34:17 +0200

    Encounters save progress

--- codex/encounter/views.py
+++ codex/encounter/views.py
@@ -27,12 +27,12 @@ def getTreasure():
     pass
 
 #def EncounterTest(request):
-def EncounterTest():
+def EncounterTest(dif):
     canon = "C"
     align = "C"
     players_level = 40
     arbitrary_num = 4
-    difficulty = 1.5 #normal
+    difficulty = dif #normal
    
     #probs = {1:range(20), 2:range(20,30), 3:range(30,45), 4:range(45,70), 5:range(70,85), 6:range(85,100)}
     
@@ -41,19 +41,6 @@ def EncounterTest():
 
     region = dungeon_probs
 
-    # Select range creatures
-    #selection = randint(0,99)
-    #i = 0
-    #probs = dungeon_probs.keys().sort()
-    #for k in probs:
-        #if selection > k:
-            #selected = region[probs[i]]
-            #break
-        #i += 1
-        
-    # Select num creatures
-    #numcreatures = randint(selected['numrange'][0], selected['numrange'][-1])
-
     # 1. filter available creatures by canon level
     creature_list = Creature.objects.filter(canon_level = canon)
     # 2. filter available creatures by alignment
@@ -61,13 +48,12 @@ def EncounterTest():
     # 3. Possible encounters
     encounter_list = {}
     base_encounterHD = players_level/arbitrary_num
-
-    
+  
     # One creature
     creature = getCreature(creature_list, base_encounterHD)
     if creature:
         encounter_list[1] = [base_encounterHD,creature]
-    print "encounter_list: ", encounter_list
+    #print "encounter_list: ", encounter_list
     # Group of creatures
     encounterHD = int(base_encounterHD * difficulty)
 
@@ -79,42 +65,34 @@ def EncounterTest():
             creature = getCreature(creature_list, hitdice)
             if creature:
                 encounter_list[ncreatures] = [hitdice,creature]
-            print "encounter_list: ", encounter_list
+            #print "encounter_list: ", encounter_list
         else:
             break
 
     probs = dungeon_probs.keys()
     probs.sort()
     found = False
-    print 'probs: ', probs
+    #print 'probs: ', probs
     while found == False:
+        # Select range creatures
         selection = randint(1,100)
-        print 'selection: ', selection
+        #print 'selection: ', selection
         i = 0
         for k in probs:
             if selection < k:
                 selected = region[probs[i]]
                 break
             i += 1
+        # Select num creatures
         numcreatures = randint(selected['numrange'][0], selected['numrange'][-1])
         print 'numcreatures, selected: ', numcreatures, selected
         try:
             encounter = encounter_list[numcreatures]
-            
             found = True
             #print 'encounter: ', encounter
         except:
             #print 'Except'
             pass
             
-    
-    #for ncreatures, prob in probs.items():
-        #if selection in prob:
-            #try:
-                #encounter = ncreatures, encounter_list[ncreatures]
-            #except:
-                #selection = randint(0,99)
-            #break
-
     #return HttpResponse(encounter)
     return encounter
