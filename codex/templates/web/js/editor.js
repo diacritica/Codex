dc3a8daa30d5d32d3110602400c660cda81109fa
django developer <django@djangodev.lan>
2012-08-04 20:08:51 +0200

    better editor

--- codex/templates/web/js/editor.js
+++ codex/templates/web/js/editor.js
@@ -2,46 +2,62 @@ dojo.require("dijit.Editor");
 
 // extra plugins
 dojo.require("dijit._editor.plugins.FontChoice");
-dojo.require("dojox.editor.plugins.TextColor");
-dojo.require("dojox.editor.plugins.Blockquote");
 dojo.require("dijit._editor.plugins.LinkDialog");
-dojo.require("dojox.editor.plugins.InsertAnchor");
-dojo.require("dojox.editor.plugins.FindReplace");
 dojo.require("dojox.editor.plugins.ShowBlockNodes");
-dojo.require("dojox.editor.plugins.PasteFromWord");
 dojo.require("dijit._editor.plugins.ViewSource");
-dojo.require("dijit._editor.plugins.FullScreen");
 dojo.require("dojox.editor.plugins.InsertEntity");
 
 // headless plugins
-dojo.require("dojox.editor.plugins.CollapsibleToolbar");
 dojo.require("dojox.editor.plugins.NormalizeIndentOutdent");
-dojo.require("dojox.editor.plugins.PrettyPrint");// let's pretty-print our HTML
+dojo.require("dojox.editor.plugins.PrettyPrint"); // let's pretty-print our HTML
 dojo.require("dojox.editor.plugins.AutoUrlLink");
 dojo.require("dojox.editor.plugins.ToolbarLineBreak");
 
 dojo.ready(function(){
-    var textareas = dojo.query("textarea");
-    if(textareas && textareas.length){
-	dojo.addClass(dojo.body(), "claro");
-	textareas.instantiate(dijit.Editor, {
-	    styleSheets: "/appmedia/style.css;/appmedia/blog/style.css",
-	    plugins: [
-		"collapsibletoolbar",
-		"fullscreen", "viewsource", "|",
-		"undo", "redo", "|",
-		"cut", "copy", "paste", "|",
-		"bold", "italic", "underline", "strikethrough", "|",
-		"insertOrderedList", "insertUnorderedList", "indent", "outdent", "||",
-		"formatBlock", "fontName", "fontSize", "||",
-		"findreplace", "insertEntity", "blockquote", "|",
-		"createLink", "insertImage", "insertanchor", "|",
-		"foreColor", "hiliteColor", "|",
-		"showblocknodes", "pastefromword",
-        // headless plugins
-		"normalizeindentoutdent", "prettyprint",
-		"autourllink", "dijit._editor.plugins.EnterKeyHandling"
-      ]
-	});
-    }
+  var textareas = dojo.query("textarea");
+  if(textareas && textareas.length){
+    dojo.addClass(dojo.body(), "claro");
+    dojo.forEach(textareas, function(textarea) {
+            
+        //Create the editor on click
+    	dojo.connect(textarea, 'click', function (event) {
+    	
+    		dojo.style(textarea, {'display' : 'none'});
+    			
+    	    var editor_div = dojo.create('div');
+    	    dojo.place(editor_div, textarea, 'after');
+    	 	dojo.style(editor_div, {
+    	 	 				 height	: '340px',
+    	 	 				 width  : '610px',
+    	 					 'float'	: 'left',});
+    	 	
+    	 
+    	 					 
+       		var editor = new dijit.Editor({
+		      //styleSheets: "/static/css/roadmap.css",
+		      plugins: [
+		        
+		        "bold", "italic", "underline", "strikethrough", "|",
+		        "insertOrderedList", "insertUnorderedList",  
+		        "formatBlock", "createLink",  "|", "viewsource",
+		        // headless plugins
+		        "normalizeindentoutdent", "prettyprint",
+		        "autourllink", "dijit._editor.plugins.EnterKeyHandling"
+		      ],
+		      onBlur: function (event) {
+		      	textarea.value = this.get('value');
+		      	//Destroy the editor on blur
+		      	editor_div.parentNode.removeChild(editor_div);
+		      	dojo.style(textarea, {'display' : 'block'});
+		      	this.destroyRecursive();
+		      }
+		    }, editor_div);
+		    
+			editor.onLoadDeferred.then(function () {
+				editor.set('value', textarea.value);
+				editor.focus();
+			})
+    	});
+    });
+  }
 });
